290a07ba5e8aaa3727c224fcd7312995bb7d3f35
Jasonwu-0803 <chufan.wu@outlook.com>
2022-04-09 11:58:52 +0800

    添加角色信息（狗日的app，commit都没法好好commit）

--- Assets/Script/EditCharacters.cs
+++ Assets/Script/EditCharacters.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class EditCharacters : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class EditCharacters : MonoBehaviour
     public InputField Name;
     public InputField Story;
 
+    //[HideInInspector]
+    public List<CharacterInfo> CharacterInfos;
+
+    private CharacterInfo curCharacter_;
+
+    public void Start()
+    {
+        CharacterInfos = new List<CharacterInfo>();
+    }
 
     public void NextButton()
     {
@@ -25,42 +35,33 @@ public class EditCharacters : MonoBehaviour
     {
         CharactersUI.SetActive(false);
         CharacterUI.SetActive(true);
-    }
 
-    public void DetectiveToggle()
-    {
-        Suspect.isOn = false;
-        Murderer.isOn = false;
-    }
+        CharacterInfo newInfo = new CharacterInfo();
+        CharacterInfos.Add(newInfo);
+        curCharacter_ = newInfo;
 
-    public void SuspectToggle()
-    {
-        Detective.isOn = false;
-        Murderer.isOn = false;
-    }
-
-    public void MurdererToggle()
-    {
-        Detective.isOn = false;
-        Suspect.isOn = false;
     }
 
     public void SaveButton()
     {
-        //To Do: Save current character
-        /*
-        int identity = CharacterClass.DETECTIVE;
-        if (Suspect.isOn == true)
+        CharacterInfo.IdentityType identity;
+        if (Detective.isOn)
         {
-            identity = CharacterClass.SUSPECT;
-        } else if (Murderer.isOn == true)
+            identity = CharacterInfo.IdentityType.Detective;
+        } else if (Suspect.isOn)
         {
-            identity = CharacterClass.MURDERER;
+            identity = CharacterInfo.IdentityType.Suspect;
+        } else
+        {
+            identity = CharacterInfo.IdentityType.Murder;
         }
+        curCharacter_.SetIdentity(identity);
         string name = Name.text;
         string story = Story.text;
-        */
-        
+
+        curCharacter_.SetName(name);
+        curCharacter_.SetStory(story);
+
         CharacterUI.SetActive(false);
         CharactersUI.SetActive(true);
     }
