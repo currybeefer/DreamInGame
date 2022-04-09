449703fbe1814ac7e44f6c1af01d7df0e28287b2
Chufan Wu <chufan.wu@outlook.com>
2022-04-09 11:36:30 +0800

    添加角色信息

---
+++ Assets/Script/CharacterInfo.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class CharacterInfo
+{
+    public enum IdentityType
+    {
+        Detective,
+        Murder,
+        Suspect
+    }
+    private string name_;
+    private IdentityType identity_;
+    private string story_;
+
+    public CharacterInfo()
+    {
+        Debug.Log("Character created");
+    }
+
+    public CharacterInfo(string name, IdentityType identity, string story)
+    {
+        name_ = name;
+        identity_ = identity;
+        story_ = story;
+    }
+
+    public string GetName()
+    {
+        return name_;
+    }
+
+    public IdentityType GetIdentity()
+    {
+        return identity_;
+    }
+
+    public string GetStory()
+    {
+        return story_;
+    }
+
+    public void SetName(string name)
+    {
+        name_ = name;
+        Debug.Log("character " + name_ + " \'s name updated");
+    }
+
+    public void SetStory(string story)
+    {
+        Debug.Log("character " + name_ + " \'s story updated");
+        story_ = story;
+    }
+
+    public void SetIdentity(IdentityType identity)
+    {
+        Debug.Log("character " + name_ + " \'s identity updated");
+        identity_ = identity;
+    }
+}
